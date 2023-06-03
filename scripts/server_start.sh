d3d2c88288b5e5e626a4fc9f7c2cccde154d20d6
njunna <nagarajujunna90@gmail.com>
2023-06-03 16:49:54 +0530

    updated

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar spring-boot-sample-app-1.0.0.jar &
+sudo java -jar spring-boot-sample-app-1.0.0.jar \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
