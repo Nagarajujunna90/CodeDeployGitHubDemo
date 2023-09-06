34b98a5a3e617c2a12a220811141a37435b27065
njunna <nagarajujunna90@gmail.com>
2023-09-06 10:20:34 +0530

    updated

--- src/main/java/com/app/SampleApplication.java
+++ src/main/java/com/app/SampleApplication.java
@@ -1,8 +1,13 @@
 package com.app;
 
+import com.app.controller.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @SpringBootApplication
 public class SampleApplication {
 
@@ -10,4 +15,19 @@ public class SampleApplication {
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
+	@GetMapping("/employee")
+	public ResponseEntity<?> getEmployee() {
+		return new ResponseEntity<>("Hello world version 2", HttpStatus.CREATED);
+	}
+
+
+	@GetMapping("/employee/{id}")
+	public ResponseEntity<?> getEmployeeById(@PathVariable Integer id) {
+		return new ResponseEntity<>("I am from get API" + id, HttpStatus.CREATED);
+	}
+
+	@PostMapping("/employee")
+	public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
+		return new ResponseEntity<>("I am from get API" + employee, HttpStatus.CREATED);
+	}
 }
---
+++ src/main/java/com/app/controller/Employee.java
@@ -0,0 +1,10 @@
+package com.app.controller;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+private Integer id;
+private String name;
+private Integer age;
+}
