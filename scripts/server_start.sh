e82fd059ce72f2f47733ba29cec6b8abbd0db2d4
njunna <nagarajujunna90@gmail.com>
2023-09-06 16:00:58 +0530

    updated with script changes

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
+mvn clean package
+#mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=9091' > /dev/null 2> /dev/null < /dev/null &
 sudo java -jar spring-boot-sample-app-1.0.0.jar \
     *.jar > /dev/null 2> /dev/null < /dev/null &
