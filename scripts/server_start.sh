a482c7721568cab0cf0637617924c73e83bdc46e
njunna <nagarajujunna90@gmail.com>
2023-09-07 09:41:07 +0530

    updated for s3 source zip and destination ec2 same

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,5 +5,5 @@ sudo unzip spring-build-demo.zip
 #mvn spring-boot:run -Drun.jvmArguments='-Dserver.port=9091' > /dev/null 2> /dev/null < /dev/null &
 #sudo java -jar target/spring-boot-sample-app-1.0.0.jar \
 #    *.jar > /dev/null 2> /dev/null < /dev/null &
-sudo java -jar spring-build-demo/spring-boot-sample-app-1.0.0.jar \
+sudo java -jar spring-boot-sample-app-1.0.0.jar \
     *.jar > /dev/null 2> /dev/null < /dev/null &
